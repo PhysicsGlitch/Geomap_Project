b8ac7140337185a4e675275b0efeee7fe1225211
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-10-15 16:15:55 -0500

    map layers working! now need legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,18 +1,84 @@
+var container = L.DomUtil.get('map');
+      if(container != null){
+        container._leaflet_id = null;
+      }
+
+function createMap(darkBaseMap) {
+  // Adding tile layer
+  var darkBaseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/dark-v10"/*"vrohm/ckg12jke42t8m19qmb4ansbby"*/,
+    accessToken: API_KEY
+  });
+
+  // LAYER CONTROL
+  // Only one base layer can be shown at a time
+  var baseMaps = {
+    "Chicago": darkBaseMap,
+  };
+
+  // Overlays that may be toggled on or off
+  var overlayMaps = {
+    Crashes: crashes,
+    Chicago: chiAreas
+  };
+
+
+  // Creating map object
+  var container = L.DomUtil.get('map'); if(container != null){ container._leaflet_id = null; }
+  var myMap = L.map("map", {
+    center: [41.8705496, -87.6239202],
+    //pitch: 9.76,
+    zoom: 12,
+    layers: [darkBaseMap]
+  });
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  // Create a legend to display information about our map
+  var info = L.control({
+    position: "bottomright"
+  });
+
+  info.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend")
+    depth = ["Crashes", "FREEZING RAIN-DRIZZLE", "CLEAR", "SNOW", "BLOWING SNOW"]
+    // labels = [];
+    div.innerHTML += "<h4>2019 Crashes</h4>"
+    div.innerHTML += '<i style="background: #BA738B"></i><span>-10 - 10</span><br>';
+    div.innerHTML += '<i style="background: #E54E8D"></i><span>10 - 30</span><br>';
+    div.innerHTML += '<i style="background: #EEBD70"></i><span>30 - 50</span><br>';
+    div.innerHTML += '<i style="background: #27016E"></i><span>50 - 70</span><br>';
+    div.innerHTML += '<i style="background: #009A9F"></i><span>70 - 90+</span><br>';
+
+    return div;
+  };
+  // Add the info legend to the map
+  info.addTo(myMap);
+
+}
 
 // **** CHICAGO AREA MAP LINES ****
 // Use this link to get the geojson data chicago area map.
 var chiMap = "static/data/chicago-community-areas.geojson";
-console.log(chiMap);
-
+// console.log(chiMap);
+var chiAreas; 
 // Grabbing our GeoJSON data..
 d3.json(chiMap, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   mapFeatures(data.features);
 });
 
-function mapFeatures(data) {
+function mapFeatures(geojson) {
   // Creating a geoJSON layer with the retrieved data
-  var chiAreas = L.geoJson(data, {
+  chiAreas = L.geoJson(geojson, {
     // Style each feature (in this case a neighborhood)
     style: function(feature) {
       return {
@@ -55,7 +121,23 @@ function mapFeatures(data) {
   createMap(chiAreas);
 
 }
+
+//**********//
+// COLORS FOR THE FEATURES AND LEGEND
+function getValue(x) {
+  if (x === "BLOWING SNOW") {
+    return "#009A9F"
+  } else if (x === "SNOW") {
+    return "#27016E"
+  } else if (x === "CLEAR") {
+    return "#EEBD70"
+  } else if (x === "FREEZING RAIN\/DRIZZLE") {
+    return "#E54E8D"
+  } else {
+    return "#BA738B"
+  }
   
+}
 
 // ADD CRASHES DATA HERE ** SIMILAR TO EARTHQAUKE MAP FROM LEAFLET PROJECT
 /* 
@@ -74,31 +156,17 @@ minified geojson Key mapping:
 */
 // Store our geojson inside crashes
 var chiCrashes = "static/data/crashes_in_2019min.geojson";
-      
+var crashes;      
 // Perform a GET request to the query URL
+
 d3.json(chiCrashes, function(data) {
   crashFeatures(data.features);
 });
   
 // ****** WHERE THE CRASH CIRCLES ARE CREATED!!****
 function crashFeatures(crashdata) {
-  //**********//
-  // COLORS FOR THE FEATURES AND LEGEND
-  function getValue(x) {
-    if (x === "BLOWING SNOW") {
-      return "#009A9F"
-    } else if (x === "SNOW") {
-      return "#27016E"
-    } else if (x === "CLEAR") {
-      return "#EEBD70"
-    } else if (x === "FREEZING RAIN\/DRIZZLE") {
-      return "#E54E8D"
-    } else {
-      return "#BA738B"
-    }
-    
-  }
-  var crashes = L.geoJSON(crashdata, {
+  
+  crashes = new L.GeoJSON(crashdata, {
     onEachFeature: function (feature, layer) {
       // Setting the marker radius for the city by passing population into the markerSize function
       
@@ -117,45 +185,6 @@ function crashFeatures(crashdata) {
   createMap(crashes)
 }
 
-function createMap(darkBaseMap) {
-  // Adding tile layer
-  var darkBaseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/dark-v10"/*"vrohm/ckg12jke42t8m19qmb4ansbby"*/,
-    accessToken: API_KEY
-  });
-
-  // LAYER CONTROL
-  // Only one base layer can be shown at a time
-  var baseMaps = {
-    "Chicago": darkBaseMap,
-  };
-
-  // Overlays that may be toggled on or off
-  var overlayMaps = {
-    Crashes: crashes,
-    ChicagoAreas: chiAreas
-  };
-
-
-  // Creating map object
-  var myMap = L.map("map", {
-    center: [41.8705496, -87.6239202],
-    //pitch: 9.76,
-    zoom: 12,
-    layers: [darkBaseMap]
-  });
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
 
 
 
