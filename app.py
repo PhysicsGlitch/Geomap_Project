3507ec4141c14816e69967e313e479f6e17c7d04
Jason Stanghelle <58093104+PhysicsGlitch@users.noreply.github.com>
2020-10-16 13:42:25 -0500

    Add files via upload
    Added code for flask app and gathering data from the mongodb data base. This code is the functional prototype to build out the back-end database for our app.
    
    The code pulls from a mongo db data base and through flask renders a json dictionary at a flask directory. Example "/dictionary" and returns a dictionary json_dict. d3 can then get data from the flask app using a call such as d3.json("/dictionary", function(json_dict) to load our data on the front-end.

--- app.py
+++ app.py
@@ -1,36 +1,27 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import traffic_accidents
-import pandas as pd
+from flask import Flask, render_template, json
+import pymongo
+from bson.json_util import dumps, loads
 
 # Create an instance of Flask
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/accident_app"
-mongo = PyMongo(app)
-accidents = mongo.db.accident_data
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    # I created an index for me mongodb beecause it was an easy collection method and also made it easy to reference
-    # it in the html file  
-    chicago_accidents = mongo.db.collection.find_one()
-    base_data = pd.read_json("basic_traffic_data")
-    mongo.db.collection.update({}, base_data, upsert=True)
-    # Return template and data
-    return render_template("index.html", chicago_accidents=chicago_accidents)
+    # I created an index for me mongodb because it was an easy collection method and also made it easy to reference
+    # it in the html file
 
-# Route that will trigger the scrape function
+    return render_template("index.html")
 
-# One function per button to update the data in our data frame
-
-@app.route("/update")
-def updater():
-    traffic_data = traffic_data.update_data()
-    mongo.db.collection.update({}, traffic_data, upsert=True)
-    return redirect("/", code=302)
-
+@app.route("/dictionary")
+def create_dictionary():
+    acdb = myclient.accident_app
+    ac_facts = acdb['accident_data'].find_one()
+    json_dict = json.loads(dumps(ac_facts))
+    return json_dict
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ chicago_accidents.js
@@ -0,0 +1,81 @@
+// Adding tile layer
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: MAP_API_KEY
+})
+
+ var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: MAP_API_KEY
+  });
+
+var fatality = L.layerGroup();
+
+d3.json("/dictionary", function(json_dict) {
+    const lat = Object.values(json_dict.fatalities.latitude);
+    const long = Object.values(json_dict.fatalities.longitude);
+     for (var i = 0; i < lat.length; i++) {
+      L.marker([lat[i], long[i]]).addTo(fatality);
+    }
+});
+
+var myMap = L.map("map", {
+  center: [41.8, -87.6],
+  zoom: 13,
+    layers: [streetmap, fatality]
+});
+
+
+var hit_run_2020 = L.layerGroup();
+d3.json("mongodb://localhost:27017/accident_app//hit_n_run_2020.json", function(data) {
+    
+    var heatArray = [];
+    const lat = Object.values(data.latitude);
+    const long = Object.values(data.longitude);
+    for (var i = 0; i < lat.length; i++) {
+       heatArray.push([lat[i], long[i]]);
+    };
+  
+  var heat = L.heatLayer(heatArray, {
+    radius: 15,
+    blur: 30
+  }).addTo(myMap);
+});
+
+var hit_run_2019 = L.layerGroup();
+d3.json("mongodb://localhost:27017/accident_app/hit_n_run_2019.json", function(data) {
+    
+    var heatArray = [];
+    const lat = Object.values(data.latitude);
+    const long = Object.values(data.longitude);
+    for (var i = 0; i < lat.length; i++) {
+       heatArray.push([lat[i], long[i]]);
+    };
+  
+  var heat = L.heatLayer(heatArray, {
+    radius: 15,
+    blur: 30
+  }).addTo(myMap);
+});
+
+var baseMaps = {
+     "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+    //Define overlay maps
+
+var overlayMaps = {
+  "Fatalities": fatality,
+    "Hit and Runs 2020": hit_run_2020,
+    "Hit and Runs 2019": hit_run_2019
+ };
+
+// This final line just adds my base maps and overlaymaps to the final plot
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
